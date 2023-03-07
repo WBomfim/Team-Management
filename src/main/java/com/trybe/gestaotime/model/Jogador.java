5d4cd164c9b6aa6586562b35d1459a24b8c4cf3e
WBomfim <willianbomfim@hotmail.com>
2023-03-07 15:44:05 -0300

    feat: Criado a entidade Jogador

--- src/main/java/com/trybe/gestaotime/model/Jogador.java
+++ src/main/java/com/trybe/gestaotime/model/Jogador.java
@@ -1,17 +1,70 @@
 package com.trybe.gestaotime.model;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToOne;
+
 /**
  * Classe Jogador.
  **/
-
+@Entity
 public class Jogador {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-  /**
-   * Atributos.
-   **/
+  private String nome;
 
-  /**
-   * Métodos.
-   **/
+  private String posicao;
+
+  @OneToOne(mappedBy = "id", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
+  @JoinColumn(name = "documento_id")
+  private Documento documento;
+
+  @ManyToOne
+  @JoinColumn(name = "time_id")
+  private Time time;
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public String getPosicao() {
+    return posicao;
+  }
+
+  public void setPosicao(String posicao) {
+    this.posicao = posicao;
+  }
+
+  public Documento getDocumento() {
+    return documento;
+  }
+
+  public void setDocumento(Documento documento) {
+    this.documento = documento;
+  }
+
+  public Time getTime() {
+    return time;
+  }
+
+  public void setTime(Time time) {
+    this.time = time;
+  }
 
 }
