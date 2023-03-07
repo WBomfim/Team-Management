759a9c474b00846251ad4677d69b74fbd85e3cb6
WBomfim <willianbomfim@hotmail.com>
2023-03-07 16:11:12 -0300

    feat: Criado a entidade Time

--- src/main/java/com/trybe/gestaotime/model/Time.java
+++ src/main/java/com/trybe/gestaotime/model/Time.java
@@ -1,17 +1,62 @@
 package com.trybe.gestaotime.model;
 
+import java.util.Set;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
 /**
  * Classe Time.
  **/
-
+@Entity
 public class Time {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-  /**
-   * Atributos.
-   **/
+  private String nome;
 
-  /**
-   * Métodos.
-   **/
+  @OneToMany(mappedBy = "time", cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+  private Set<Jogador> jogadores;
+
+  @ManyToMany(cascade = { CascadeType.PERSIST, CascadeType.MERGE })
+  @JoinTable(name = "time_torcedor", joinColumns = { @JoinColumn(name = "time_id") },
+      inverseJoinColumns = { @JoinColumn(name = "torcedor_id") })
+  private Set<Torcedor> torcedores;
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public Set<Jogador> getJogadores() {
+    return jogadores;
+  }
+
+  public void setJogadores(Jogador jogador) {
+    this.jogadores.add(jogador);
+  }
+
+  public Set<Torcedor> getTorcedores() {
+    return torcedores;
+  }
+
+  public void setTorcedores(Torcedor torcedor) {
+    this.torcedores.add(torcedor);
+  }
 
 }
