2af0002813eca7f91ee68a5b121e9d25e69705ea
WBomfim <willianbomfim@hotmail.com>
2023-03-07 15:03:31 -0300

    feat: Criado a entidade Documento

--- src/main/java/com/trybe/gestaotime/model/Documento.java
+++ src/main/java/com/trybe/gestaotime/model/Documento.java
@@ -1,16 +1,51 @@
 package com.trybe.gestaotime.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 /**
  * Classe Documento.
  */
-
+@Entity
 public class Documento {
-  /**
-   * Atributos.
-   */
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-  /**
-   * Métodos.
-   */
+  private String cpf;
+
+  private String numeroDaCdt;
+
+  private String numeroCbf;
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getCpf() {
+    return cpf;
+  }
+
+  public void setCpf(String cpf) {
+    this.cpf = cpf;
+  }
+
+  public String getNumeroDaCdt() {
+    return numeroDaCdt;
+  }
+
+  public void setNumeroDaCdt(String numeroDaCdt) {
+    this.numeroDaCdt = numeroDaCdt;
+  }
+
+  public String getNumeroCbf() {
+    return numeroCbf;
+  }
+
+  public void setNumeroCbf(String numeroCbf) {
+    this.numeroCbf = numeroCbf;
+  }
 
 }
