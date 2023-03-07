c9f61d7180f849f293124b677cac0b4646b8ffb7
WBomfim <willianbomfim@hotmail.com>
2023-03-07 16:28:01 -0300

    feat: Criado a entidade Torcedor

--- src/main/java/com/trybe/gestaotime/model/Torcedor.java
+++ src/main/java/com/trybe/gestaotime/model/Torcedor.java
@@ -1,17 +1,45 @@
 package com.trybe.gestaotime.model;
 
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
 /**
  * Classe Torcedor.
  **/
-
+@Entity
 public class Torcedor {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-  /**
-   * Atributos.
-   **/
+  private String nome;
 
-  /**
-   * Métodos.
-   **/
+  @ManyToMany(mappedBy = "torcedores")
+  private Set<Time> times;
+
+  public Long getId() {
+    return id;
+  }
+
+  public String getNome() {
+    return nome;
+  }
+
+  public void setNome(String nome) {
+    this.nome = nome;
+  }
+
+  public Set<Time> getTimes() {
+    return times;
+  }
+
+  public void setTimes(Time time) {
+    this.times.add(time);
+  }
 
 }
