303ad2c6c0d6d18eb37c8837759fe99df801b0cc
WBomfim <willianbomfim@hotmail.com>
2023-03-08 18:27:17 -0300

    fix: Corrigido alertas do checkstyle

--- src/main/java/com/trybe/gestaotime/Run.java
+++ src/main/java/com/trybe/gestaotime/Run.java
@@ -1,25 +1,26 @@
 package com.trybe.gestaotime;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import com.trybe.gestaotime.dao.AplicacaoBD;
+import com.trybe.gestaotime.dao.AplicacaoBd;
 import com.trybe.gestaotime.model.Documento;
 import com.trybe.gestaotime.model.Jogador;
 import com.trybe.gestaotime.model.Time;
 import com.trybe.gestaotime.model.Torcedor;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Classe Run.
+ * 
  */
-
 public class Run {
+  private static AplicacaoBd aplicacaoBd = new AplicacaoBd();
+  private static List<Jogador> jogadores = new ArrayList<Jogador>();
+  private static List<Torcedor> torcedores = new ArrayList<Torcedor>();
 
+  /**
+   * Método Principal.
+   */
   public static void main(String[] args) {
-    AplicacaoBD aplicacaoBD = new AplicacaoBD();
-    List<Jogador> jogadores = new ArrayList<Jogador>();
-    List<Torcedor> torcedores = new ArrayList<Torcedor>();
-
     Time time = new Time();
     time.setNome("Palmeiras");
 
@@ -32,21 +33,21 @@ public class Run {
     documento2.setCpf("12345678911");
     documento2.setNumeroCarteiraTrabalho("12345678911-1");
     documento2.setNumeroCbf("12346");
-
+    
     Jogador jogador1 = new Jogador();
     jogador1.setNome("Ronaldo");
     jogador1.setPosicao("Atacante");
     jogador1.setDocumento(documento1);
     jogador1.setTime(time);
     jogadores.add(jogador1);
-    
+
     Jogador jogador2 = new Jogador();
     jogador2.setNome("Rivaldo");
     jogador2.setPosicao("Atacante");
     jogador2.setDocumento(documento2);
     jogador2.setTime(time);
     jogadores.add(jogador2);
-
+    
     time.setJogadores(jogadores);
 
     Torcedor torcedor = new Torcedor();
@@ -56,6 +57,11 @@ public class Run {
 
     time.setTorcedores(torcedores);
 
-    aplicacaoBD.timeDao().salvar(time);
+    aplicacaoBd.documentoDao().salvar(documento1);
+    aplicacaoBd.documentoDao().salvar(documento2);
+    aplicacaoBd.jogadorDao().salvar(jogador1);
+    aplicacaoBd.jogadorDao().salvar(jogador2);
+    aplicacaoBd.torcedorDao().salvar(torcedor);
+    aplicacaoBd.timeDao().salvar(time);
   }
 }
--- src/main/java/com/trybe/gestaotime/dao/AplicacaoBd.java
+++ src/main/java/com/trybe/gestaotime/dao/AplicacaoBd.java
@@ -1,16 +1,19 @@
 package com.trybe.gestaotime.dao;
 
 /**
- * Classe AplicacaoBD.
+ * Classe AplicacaoBd.
+ * 
  **/
-
-public class AplicacaoBD {
+public class AplicacaoBd {
   private TimeDao timeDao;
   private JogadorDao jogadorDao;
   private TorcedorDao torcedorDao;
   private DocumentoDao documentoDao;
 
-  public AplicacaoBD() {
+  /**
+   * Construtor da classe AplicacaoBd.
+   */
+  public AplicacaoBd() {
     this.timeDao = new TimeDao();
     this.jogadorDao = new JogadorDao();
     this.torcedorDao = new TorcedorDao();
