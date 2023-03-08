7c652c199d546674262bd50863b9e18f10548639
WBomfim <willianbomfim@hotmail.com>
2023-03-08 18:45:24 -0300

    chore: Excluído import desnecessário

--- src/main/java/com/trybe/gestaotime/model/Time.java
+++ src/main/java/com/trybe/gestaotime/model/Time.java
@@ -1,7 +1,6 @@
 package com.trybe.gestaotime.model;
 
 import java.util.List;
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
